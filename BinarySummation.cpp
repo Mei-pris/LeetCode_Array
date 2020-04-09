c780c27aab2e1240e5ebd78027970c5ca7a982c8
Mei <bmeiei_zhang@163.com>
2020-04-09 22:39:25 +0800

    二进制相加：使用char字符的ASCII码进行计算，还使用了反序函数

--- BinarySummation.cpp
+++ BinarySummation.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
-    string addBinary(string a, string b){
+   /* string addBinary(string a, string b){
         string c;
         int n1 = a.size()-1, n2 = b.size()-1,n;
         if (n1 >= n2) { c=a; n = n1; }
@@ -25,9 +26,9 @@ public:
         }        
         if (newflag == '1') return "1" + c;
         else   return c;
-    }
-
-    char Add(char m, char n,char &upflag) {
+    }*/
+  
+    /*char Add(char m, char n,char &upflag) {
         char res='0';
         if (m =='0') {
             if (n == '0') { res = '0'; return res;}
@@ -38,11 +39,31 @@ public:
             else { res='0'; upflag='1'; return res;}
 
         }
+    }*/
+
+    string addBinary(string a, string b) {
+        string c;
+        int sum = 0, n1 = a.size() - 1, n2 = b.size()-1,up=0;
+        while (n1>=0 ||n2>=0) {
+            sum = up;
+            sum += (n1>=0?a[n1]-'0':0);
+            sum += (n2>=0?b[n2]-'0':0);
+            string cc = to_string(sum % 2);
+            c.append(cc);
+            up = sum / 2;
+            --n1; --n2;
+        }
+        if (up == 1) c.append("1");
+        reverse(c.begin(),c.end());
+        return c;
+
     }
+
+
 
 };
 int main() {
-    string s1 = "0",s2="0";
+    string s1 = "1011",s2="11";
     Solution mysolution;
     string ans = mysolution.addBinary(s1, s2);
     cout << ans << endl;
